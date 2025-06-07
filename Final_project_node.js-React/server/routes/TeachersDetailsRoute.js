3bff090dd219717a54030e8ad56f1bde4afcfda2
Tehila-m <tehila40996@gmail.com>
2025-06-07 23:16:42 +0300

    Delete Final_project_node.js-React directory

--- Final_project_node.js-React/server/routes/TeachersDetailsRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const TeachersDetailsController = require("../controllers/teachersDetailsController")
-
-router.get("/", TeachersDetailsController.getAllTeachers)
-router.post("/", TeachersDetailsController.createTeacher)
-router.get('/institution/:institutionId', TeachersDetailsController.getTeachersByInstitution);
-
-module.exports = router
